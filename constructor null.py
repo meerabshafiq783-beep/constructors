37470a1154b1e381259f0316b1f341d3f4cd38db
Meerab Shafiq <meerabshafiq783@gmail.com>
2025-08-04 07:24:56 +0500

    Add files via upload

---
+++ constructor null.py
@@ -0,0 +1,10 @@
+class Triangle:
+    def __init__(Self):
+        Self.x
+        Self.y
+        Self.z
+        T1= Triangle("15","15","15")
+      
+        print("Value of x is",T1 .x)
+        print("Value of y is",T1 .y)
+        print("Value of z is",T1 .z)

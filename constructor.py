1801d11546709167f34fe24a9580f3d14ade4f3c
Meerab Shafiq <meerabshafiq783@gmail.com>
2025-08-04 07:25:28 +0500

    Add files via upload

---
+++ constructor.py
@@ -0,0 +1,11 @@
+class Pen:
+           
+           def __init__(Self, type, colour):
+             Self.type=type
+             Self.colour=colour
+          
+P1 = Pen( "Dollar", "Black")
+
+print(P1 .type)
+print(P1 .colour)
+
